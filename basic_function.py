a0ef777215f5d94c84e08085b035ce2684a02b34
unknown <angelesianlloyd_bist@gmail.com>
2025-02-20 14:13:24 +0800

    Submitting Python Week 04 - Laboratory # 16

---
+++ basic_function.py
@@ -0,0 +1,11 @@
+# Ian Lloyd Santiago Angeles
+# ITELEC2
+# Laboratory #16 – Guided Coding Exercise: Basic Function Definition and Calling 
+
+def greet():
+    """Prints a greeting message."""  
+
+    print("Hello, Python Students!")  
+
+
+greet()
